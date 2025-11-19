bb9b97ba75cfcf8549267311a17875d5c882f623
Mads Krabbe <spiligen1@gmail.com>
2025-11-19 09:46:05 +0100

    Added Produce byte

--- src/main/java/org/example/Factory.java
+++ src/main/java/org/example/Factory.java
@@ -1,12 +1,23 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Factory
 {
     public boolean produceRandomBit()
     {
+    }
 
-
-
-
+    public byte produceByte(ArrayList<Boolean> bits)
+    {
+        byte byteVal = 0;
+        for (int i = bits.size()-1 ;i >=0; i--)
+        {
+            if(bits.get(i))
+            {
+                byteVal += Math.pow(2.0, i);
+            }
+        }
+        return byteVal;
     }
 }
