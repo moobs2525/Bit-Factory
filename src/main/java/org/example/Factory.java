8bba4358604d10788a2f00392556083cd000aae0
Mikkelno1 <mikkelnorinder@live.dk>
2025-11-19 09:12:05 +0100

    Added produceRandomBit

--- src/main/java/org/example/Factory.java
+++ src/main/java/org/example/Factory.java
@@ -1,12 +1,16 @@
 package org.example;
+import java.util.Random;
 
 public class Factory
 {
-    public boolean produceRandomBit()
-    {
+    public boolean produceRandomBit(){
+        Random random = new Random();
+
+        int random2 = random.nextInt(0,2);
+
+        System.out.println(random2);
 
 
-
-
+        return true;
     }
 }
