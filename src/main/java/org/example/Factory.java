eb8f121e71d6361c0d1cbd09fc2426e93db75f47
Mikkelno1 <mikkelnorinder@live.dk>
2025-11-21 08:52:30 +0100

    Simplified produceRandomBits method Added arraylist

--- src/main/java/org/example/Factory.java
+++ src/main/java/org/example/Factory.java
@@ -4,25 +4,24 @@ import java.util.Random;
 
 import java.util.ArrayList;
 
-public class Factory
-{
-    public boolean produceRandomBits() {
+public class Factory {
+
+    public ArrayList<Boolean> produceRandomBits() {
+        ArrayList<Boolean> bits = new ArrayList<>();
         Random random = new Random();
-        return random.nextInt(0, 2) == 1;
-    public boolean produceRandomBit()
-    {
+        bits.add(random.nextInt(0,2) == 1);
+        return bits;
     }
 
-    public byte produceByte(ArrayList<Boolean> bits)
-    {
-        byte byteVal = 0;
-        for (int i = bits.size()-1 ;i >=0; i--)
+        public byte produceByte (ArrayList < Boolean > bits)
         {
-            if(bits.get(i))
-            {
-                byteVal += Math.pow(2.0, i);
+            byte byteVal = 0;
+            for (int i = bits.size() - 1; i >= 0; i--) {
+                if (bits.get(i)) {
+                    byteVal += Math.pow(2.0, i);
+                }
             }
+            return byteVal;
         }
-        return byteVal;
     }
-}
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,10 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
     public static void main(String[] args)
     {
         Factory factory = new Factory();
-        //factory.produceRandomBit();
         factory.produceRandomBits();
 
     }
