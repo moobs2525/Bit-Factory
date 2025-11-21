830e63b5b1710b6d87ac0bda82be7f7519b6d516
Mads Krabbe <spiligen1@gmail.com>
2025-11-21 08:58:36 +0100

    Added makeChars

--- src/main/java/org/example/Factory.java
+++ src/main/java/org/example/Factory.java
@@ -4,26 +4,38 @@ import java.util.Random;
 
 import java.util.ArrayList;
 
-public class Factory {
+public class Factory
+{
 
-    public ArrayList<Boolean> produceRandomBits() {
+    public ArrayList<Boolean> produceRandomBits()
+    {
         ArrayList<Boolean> bits = new ArrayList<>();
         Random random = new Random();
-        for (int i = 0; i <= 8; i++){
-            bits.add(random.nextInt(0,2) == 1);
+        for (int i = 0; i <= 8; i++)
+        {
+            bits.add(random.nextInt(0, 2) == 1);
         }
         return bits;
     }
 
-        public byte produceByte (ArrayList < Boolean > bits)
+    public byte produceByte(ArrayList<Boolean> bits)
+    {
+        byte byteVal = 0;
+        for (int i = bits.size() - 1; i >= 0; i--)
         {
-            byte byteVal = 0;
-            for (int i = bits.size() - 1; i >= 0; i--) {
-                if (bits.get(i)) {
-                    byteVal += Math.pow(2.0, i);
-                }
+            if (bits.get(i))
+            {
+                byteVal += Math.pow(2.0, i);
             }
-            return byteVal;
         }
+        return byteVal;
     }
+
+
+    public void makeChars(int byteValue)
+    {
+        char character = (char) byteValue;
+        System.out.println("Character: " + character);
+    }
+}
 
