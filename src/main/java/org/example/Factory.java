e272bb074d04d597e6f2a83982bb2031f6d199fd
Mikkelno1 <mikkelnorinder@live.dk>
2025-11-21 08:32:27 +0100

    Simplified produceRandomBits method

--- src/main/java/org/example/Factory.java
+++ src/main/java/org/example/Factory.java
@@ -4,22 +4,8 @@ import java.util.Random;
 
 public class Factory
 {
-    public void produceRandomBit(){
+    public boolean produceRandomBits() {
         Random random = new Random();
-        ArrayList<Boolean> bits = new ArrayList<>();
-
-
-        for (int i = 0; i <= 8; i++){
-            int randomNumber = random.nextInt(0,2);
-            boolean numberBit = true;
-
-            if (randomNumber == 1){
-                bits.add(numberBit);
-            } else {
-                numberBit = false;
-                bits.add(numberBit);
-            }
-        }
-        System.out.println(bits);
+        return random.nextInt(0, 2) == 1;
     }
 }
